4814e7542644f110994a01ca4fca72135a5c9eb6
Reed <reedperry@users.noreply.github.com>
2014-11-12 22:04:43 -0700

    Get crud request handler reading event data from json and storing entities in the datastore.

--- crud.go
+++ crud.go
@@ -2,11 +2,16 @@ package moneta
 
 import (
 	"appengine"
+	"appengine/datastore"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"time"
 )
+
+const EVENT_KIND = "event"
 
 func init() {
 	http.HandleFunc("/data", crud)
@@ -22,30 +27,76 @@ func crud(w http.ResponseWriter, r *http.Request) {
 }
 
 func doPost(w http.ResponseWriter, r *http.Request) {
-
 	c := appengine.NewContext(r)
-	data, err := getRequestData(r)
-	if err == nil {
-		for k, v := range *data {
-			if k == "kind" {
-				kind := v
-				c.Infof("Data has kind %s", kind)
+
+	entity := new(event)
+	if err := readEntity(r, entity); err == nil {
+		if err := assertValidKind(entity, c); err == nil {
+			key := datastore.NewIncompleteKey(c, EVENT_KIND, nil)
+			if k, err := datastore.Put(c, key, entity); err == nil {
+				c.Infof("Stored entity with key.String ", k.String())
+				c.Infof("Stored entity with key.StringID ", k.StringID())
+				c.Infof("Stored entity with key.IntId ", k.IntID())
+				c.Infof("Stored entity with key.Kind ", k.Kind())
+				resp := response{k.String(), nil, nil}
+				if json, err := json.Marshal(resp); err == nil {
+					w.Header().Set("Content-Type", "application/json")
+					w.Write(json)
+				} else {
+					handleError(w, err, &c)
+				}
+			} else {
+				handleError(w, err, &c)
 			}
+		} else {
+			handleError(w, err, &c)
 		}
 	} else {
-		c.Errorf(err.Error())
+		handleError(w, err, &c)
 	}
 }
 
-func getRequestData(r *http.Request) (*map[string]interface{}, error) {
+func handleError(w http.ResponseWriter, err error, c *appengine.Context) {
+	http.Error(w, err.Error(), http.StatusInternalServerError)
+}
+
+func readEntity(r *http.Request, entity *event) error {
 	defer r.Body.Close()
 
-	var data map[string]interface{}
-
-	err := json.NewDecoder(r.Body).Decode(&data)
-	if err != nil {
-		return nil, errors.New("Couldn't get valid JSON object from request body.")
+	var body []byte
+	body, readErr := ioutil.ReadAll(r.Body)
+	if readErr != nil {
+		return readErr
 	}
 
-	return &data, nil
+	err := json.Unmarshal(body, entity)
+	if err != nil {
+		return errors.New("Couldn't get valid JSON object from request body.")
+	}
+
+	return nil
+}
+
+func assertValidKind(entity *event, c appengine.Context) error {
+	kind := entity.Kind
+	if kind != "expense" && kind != "credit" {
+		return errors.New("Invalid kind")
+	} else {
+		c.Infof("Data has kind %s", kind)
+		return nil
+	}
+}
+
+type event struct {
+	Amount  float64   `json:"amount"`
+	Comment string    `json:"comment"`
+	Date    time.Time `json:"date"`
+	Kind    string    `json:"kind"`
+	User    string    `json:"user"`
+}
+
+type response struct {
+	Key   string                 `json:"key"`
+	Error string                 `json:"error"`
+	Data  map[string]interface{} `json:"data"`
 }
